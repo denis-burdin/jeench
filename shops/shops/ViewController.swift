68929a06e8b7ea8cfe7d3c07c1b46088f5fe31d2
Dennis Burdin <dburdin@accesssoftek.com>
2017-08-08 11:01:49 +0300

    initial commit

---
+++ shops/shops/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shops
+//
+//  Created by Dennis Burdin on 08.08.17.
+//  Copyright © 2017 jeench. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
